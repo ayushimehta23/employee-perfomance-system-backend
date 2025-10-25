5c6cf1741d4fc18abc1caf7651395bf2974b9e66
Ayushi Mehta <ayushimehta2342@gmail.com>
2025-10-25 12:50:38 +0530

    Modify create users API

--- epes/core/serializers.py
+++ epes/core/serializers.py
@@ -1,20 +1,23 @@
-# core/serializers.py
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'role']
+        fields = ['id', 'username', 'email', 'password', 'role', 'first_name', 'last_name']
 
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
             email=validated_data['email'],
-            role=validated_data['role']
+            role=validated_data['role'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
         user.set_password(validated_data['password'])
         user.save()
